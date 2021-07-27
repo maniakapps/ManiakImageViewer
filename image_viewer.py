ebe9a33d5f0b9543a1188f4e2161e2c5ea4c676c
maniakapps <ti15110264@itess.edu.mx>
2021-07-27 06:43:25 -0500

    GLPv3 license added

--- image_viewer.py
+++ image_viewer.py
@@ -7,8 +7,7 @@
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# the Free Software Foundation GPLv3
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,7 +15,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, see <http://www.gnu.org/licenses/>.
+# along with this program; if not, see <https://www.gnu.org/licenses/old-licenses/gpl-3.0.en.html>.
 
 import getpass
 import PIL.Image
